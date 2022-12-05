bae93695b3abc76afc6b1bfc29676addb2731e6d
mustafacicek <mustafaciceknote8@gmail.com>
2022-12-05 20:06:44 +0300

    first commit

---
+++ src/main/java/com/me/springboot3todoapp/SpringBoot3TodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.me.springboot3todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3TodoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3TodoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/me/springboot3todoapp/SpringBoot3TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.springboot3todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3TodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
